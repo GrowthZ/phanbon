eec64f6cc172d10cf4e5a1ad4e198515ddffa1b6
Hoang Du <hoangdu.tpg@gmail.com>
2026-07-08 22:15:08 +0700

    feat: update hotline and zalo contact number to 0397282212

--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,9 @@ export const BRAND = {
   name: "Men Nhà Nông",
   productName: "Bộ men vi sinh ủ chín thức ăn chăn nuôi",
   company: "Men Nhà Nông",
-  hotline: "0969 813 880",
-  zalo: "0969813880",
-  zaloUrl: "https://zalo.me/0969813880",
+  hotline: "0397 282 212",
+  zalo: "0397282212",
+  zaloUrl: "https://zalo.me/0397282212",
   region: "Giao hàng toàn quốc",
   stats: "Chuyên gia hỗ trợ kỹ thuật tận tình"
 };
